4e6075678d81fc1b8d870f39df11df766e8006eb
Carlos Carbajal <carbajal.carlos@hotmail.com>
2018-06-13 16:21:15 +0100

    adding a filtering stage to prevent data corruption

--- 1_data_input.R
+++ 1_data_input.R
@@ -16,6 +16,60 @@ di_raw <- readFiles("0_input/di_wos_001.bib", "0_input/di_wos_002.bib", "0_input
 di <- convert2df(file = di_raw, dbsource = "isi", format = "bibtex")
 #proc.time() - ptm
 
+# ----- Filtering interest articles -----
+# # Extracting unique data
+# entry.type <- as.data.frame(table(di$DT), stringsAsFactors = FALSE)
+# entry.details <- as.data.frame(table(di$DT2), stringsAsFactors = FALSE)#; entry.details
+
+# 
+# 
+# write.csv(x = sub_cat, file = "0_process/subjects.csv")
+# 
+# # Generation of the list <-
+# index <- grep(pattern = "innov", x = di)
+
+# Filtering entry type
+lista <- c("ARTICLE", "ARTICLE; BOOK CHAPTER", "ARTICLE; PROCEEDINGS PAPER", "BOOK", 
+          "EDITORIAL MATERIAL", "PROCEEDINGS PAPER")
+di_fil <- di[which(is.element(el = di$DT2, set = lista)), ]
+# Filtering by title
+index.a <- grep(pattern = "innov", x = di_fil$TI, ignore.case = TRUE)
+index.b <- grep(pattern = "digit", x = di_fil$TI, ignore.case = TRUE)
+index <- index.a[is.element(el = index.a, set = index.b)]
+# Filtering by abstract
+index.a <- grep(pattern = "innov", x = di_fil$AB, ignore.case = TRUE)
+index.b <- grep(pattern = "digit", x = di_fil$AB, ignore.case = TRUE)
+index <- unique(c(index, index.a[is.element(el = index.a, set = index.b)]))
+# Filtering by author keywords
+index.a <- grep(pattern = "innov", x = di_fil$DE, ignore.case = TRUE)
+index.b <- grep(pattern = "digit", x = di_fil$DE, ignore.case = TRUE)
+index <- unique(c(index, index.a[is.element(el = index.a, set = index.b)]))
+# Filtering by aggregator keywords
+index.a <- grep(pattern = "innov", x = di_fil$ID, ignore.case = TRUE)
+index.b <- grep(pattern = "digit", x = di_fil$ID, ignore.case = TRUE)
+index <- unique(c(index, index.a[is.element(el = index.a, set = index.b)]))
+# Filtering by subject categories
+index.a <- grep(pattern = "innov", x = di_fil$SC, ignore.case = TRUE)
+index.b <- grep(pattern = "digit", x = di_fil$SC, ignore.case = TRUE)
+index <- unique(c(index, index.a[is.element(el = index.a, set = index.b)]))
+di_fil <- di_fil[index, ]
+
+#save.set <- di_fil
+#di_fil <- save.set
+
+# Replacing general categories 
+categories <- read.csv(file = "0_process/subjects.csv", header = TRUE, stringsAsFactors = FALSE)
+for (i in 1:nrow(categories)) {
+  index <- grep(pattern = categories$unique[i], x = di_fil$SC, fixed =  TRUE, ignore.case = TRUE)
+  di_fil$SC[index] <- gsub(pattern = categories$unique[i], replacement = categories$global[i], x = di_fil$SC[index],
+                           fixed = TRUE, ignore.case = TRUE)
+}
+
+
+# Extracting subject categories
+sub_cat <- as.data.frame(table(trimws(unlist(strsplit(x = di_fil$SC, split = ";")), which = "both")), stringsAsFactors = FALSE)
+sub_cat <- sub_cat[order(sub_cat$Freq, decreasing = TRUE), ]
+
 # ----- Creating output files -----
 # Removing temporary files
 if(is.element(el = "ptm", set = ls())) { rm(ptm) }
@@ -23,3 +77,4 @@ if(is.element(el = "ptm", set = ls())) { rm(ptm) }
 # # Saving the rare.comics subset
 save(file = "0_process/di_raw.Rdata", list = "di_raw")
 save(file = "0_process/di_df.Rdata", list = "di")
+save(file = "0_process/di_fil.Rdata", list = "di_fil")
